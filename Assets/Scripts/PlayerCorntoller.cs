764d676c8f1e9e7eed662455ca83a56dbd6af191
JayGagup <jose_aguilar131@hotmail.com>
2023-09-11 14:44:26 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerCorntoller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCorntoller : MonoBehaviour
+{
+    public float speed;
+
+    private float HorizontalInput;
+    private float VerticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * HorizontalInput);
+        VerticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * VerticalInput);
+    }
+}
